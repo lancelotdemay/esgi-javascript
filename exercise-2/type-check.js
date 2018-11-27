a096fd4ba6fff52a5cfc8320a495cb3cd79481f9
Lancelot Demay <lde@frianbiz.com>
2018-11-27 16:54:28 +0100

    modified type_check_v2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,3 +1,5 @@
+console.log(type_check_v2({bar: "foo"}, {type: "object", value: {bar: "foo"}}))
+
 function type_check_v1(input, type) {
     if (input === null && type === "null" || typeof input === undefined && type === "undefined") {
         return true
@@ -16,19 +18,20 @@ function type_check_v1(input, type) {
 }
 
 function type_check_v2(arg1, arg2) {
-    if (arg2["type"] && type_check_v1(arg1, arg2["type"])) {
-        if (arg2["value"]) {
-            return arg1 == arg2["value"]
-        }
-        return true
-    }
 
     if (arg2["enum"]) {
         for (i in arg2["enum"]) {
             if (arg2["enum"][i] === arg1) {
-                return type_check_v1(arg1, typeof arg2["enum"][i])
+                return type_check_v1(arg1, arg2["enum"][i])
             }
         }
+    }
+
+    if (arg2["type"] && type_check_v1(arg1, arg2["type"]) && !arg2["enum"]) {
+        if (arg2["value"]) {
+            return JSON.stringify(arg1) === JSON.stringify(arg2["value"])
+        }
+        return true
     }
 
     return false
