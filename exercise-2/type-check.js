aecc94f5d684a984e697bebe61636dceda1e2e82
Lancelot Demay <lde@frianbiz.com>
2018-11-27 15:05:04 +0100

    modified execise-2 type_check_v1

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,5 +1,15 @@
+var undefinedVal = undefined
+
+console.log("null : " ,type_check_v1(null, "null"))
+console.log("object : " ,type_check_v1({ "test": "test"}, "object"))
+console.log("array : " ,type_check_v1([], "array"))
+console.log("number : " ,type_check_v1(1, "number"))
+console.log("string : " ,type_check_v1("salut", "string"))
+console.log("undefined :", type_check_v1(undefinedVal, "undefined"))
+console.log("function : ", type_check_v1(myFunction, "function"))
+
 function type_check_v1(input, type) {
-    if (input === null && type === "null" || input === undefined && type === "undefined") {
+    if (input === null && type === "null" || typeof input === undefined && type === "undefined") {
         return true
     } 
     if (typeof input === type) {
